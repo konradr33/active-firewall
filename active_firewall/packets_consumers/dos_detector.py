b23997ae0d795e36a28c59a3a469227274e2f2db
Konrad Radoń <konradradon@gmail.com>
2021-05-22 12:43:37 +0200

    Refactor, add sample test

--- active_firewall/packets_consumers/dos_detector.py
+++ active_firewall/packets_consumers/dos_detector.py
@@ -5,12 +5,13 @@ from active_firewall.packets_consumers.packets_consumer import PacketsConsumer
 
 class DosDetector(PacketsConsumer):
 
-    def __init__(self, allowed_packets_per_second, large_packet_size, allowed_large_packets_per_second, iptables_adapter):
+    def __init__(self, allowed_packets_per_second, large_packet_size, allowed_large_packets_per_second,
+                 iptables_adapter):
         self.iptables_adapter = iptables_adapter
         self.start = time.time()
-        self.packet_cnt = {}              
+        self.packet_cnt = {}
         self.allowed_packets_per_second = allowed_packets_per_second
-        self.large_packet_cnt = {}  
+        self.large_packet_cnt = {}
         self.large_packet_size = large_packet_size
         self.allowed_large_packets_per_second = allowed_large_packets_per_second
 
@@ -27,18 +28,18 @@ class DosDetector(PacketsConsumer):
             print("Alert DoS: " + ip + " " + str(self.packet_cnt[ip]))
             self.iptables_adapter.add_rule_with_timeout(["-s", ip])
 
-    def consume_packet(self, packet):        
-        if hasattr(packet, 'ip'):                	
-            if int(packet.length) >= self.large_packet_size:            	
-            	if packet.ip.src in self.large_packet_cnt:
-                	self.large_packet_cnt[packet.ip.src] += 1
-            	else:
-                	self.large_packet_cnt[packet.ip.src] = 1
+    def consume_packet(self, packet):
+        if hasattr(packet, 'ip'):
+            if int(packet.length) >= self.large_packet_size:
+                if packet.ip.src in self.large_packet_cnt:
+                    self.large_packet_cnt[packet.ip.src] += 1
+                else:
+                    self.large_packet_cnt[packet.ip.src] = 1
             if int(packet.length) < self.large_packet_size:
-            	if packet.ip.src in self.packet_cnt:
-                	self.packet_cnt[packet.ip.src] += 1
-            	else:
-                	self.packet_cnt[packet.ip.src] = 1
+                if packet.ip.src in self.packet_cnt:
+                    self.packet_cnt[packet.ip.src] += 1
+                else:
+                    self.packet_cnt[packet.ip.src] = 1
         if time.time() - self.start >= 1:
             self.__find_alerts()
             self.__reset()
