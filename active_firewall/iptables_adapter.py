a497a8fe9ee01b2848058e72c33f9c6b776e4e67
Konrad Radoń <konradradon@gmail.com>
2021-05-24 23:22:32 +0200

    Add extending the iptables rules

--- active_firewall/iptables_adapter.py
+++ active_firewall/iptables_adapter.py
@@ -5,7 +5,7 @@ from threading import Timer
 
 class IptablesAdapter:
     """
-    Class that can apply rules on ip tables tool. It's being an adapter.
+    Adapter class that can apply rules on ip tables tool.
     """
 
     def __init__(self, chain, clear_on_exit=False):
@@ -17,6 +17,7 @@ class IptablesAdapter:
         :param clear_on_exit: if flag raised, chain will be flushed after application exit
         :type clear_on_exit: bool, optional
         """
+        self.applied_rules_timers = dict()
         self.chain = chain
         self.clear_on_exit = clear_on_exit
         atexit.register(self.__flush_chain, self.chain, self.clear_on_exit)
@@ -24,15 +25,26 @@ class IptablesAdapter:
     def add_rule_with_timeout(self, rule, timeout):
         """
         Add a rule to iptables. After a defined time, it will be automatically deleted.
+        If a rule is already applied, new timer will replace old one.
 
         :param rule: list of string defining rule that is applied into iptables
         :type rule: list
         :param timeout: time after which the rule will be revoked
         :type timeout: int
         """
+        rule_id = ''.join(rule)
+
         if self.add_rule(rule):
             t = Timer(timeout, self.delete_rule, [rule])
+            self.applied_rules_timers[rule_id] = t
             t.start()
+        else:
+            if rule_id in self.applied_rules_timers:
+                old_t = self.applied_rules_timers[rule_id]
+                old_t.cancel()
+                t = Timer(timeout, self.delete_rule, [rule])
+                self.applied_rules_timers[rule_id] = t
+                t.start()
 
     def add_rule(self, rule):
         """
@@ -62,18 +74,22 @@ class IptablesAdapter:
 
     def delete_rule(self, rule):
         """
-        Removes a rule from iptables.
+        Removes a rule from iptables. Deletes timer if exist.
 
         :param rule: list of string defining rule that is removed from iptables
         :type rule: list
         """
+        rule_id = ''.join(rule)
+        if rule_id in self.applied_rules_timers:
+            del self.applied_rules_timers[rule_id]
+
         delete_rule = [self.chain, "-j", "DROP"] + rule
         subprocess.call(["sudo", "iptables", "-D"] + delete_rule)
 
     @staticmethod
     def __check_if_rule_exists(rule):
         """
-        Check if rule is already applied to iptable.
+        Check if rule is already applied to iptables.
 
         :param rule: list of string defining rule that is removed from iptables
         :type rule: list
